c7a8a8028b47be58a401da3d92c6b8211c22b10f
Jaali <jmasteremerald@gmail.com>
2024-10-22 23:39:52 -0400

    commit

---
+++ Parks/ParkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ParkDetailView.swift
+//  Parks
+//
+//  Created by Jaali Patterson on 10/22/24.
+//
+
+import SwiftUI
+
+struct ParkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkDetailView()
+}
---
+++ Parks/ParksResponse.swift
@@ -0,0 +1,8 @@
+//
+//  ParksResponse.swift
+//  Parks
+//
+//  Created by Jaali Patterson on 10/22/24.
+//
+
+import Foundation
