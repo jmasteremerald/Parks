c40362a121b40a87614c4d50c14680372cdb0014
Jaali <jmasteremerald@gmail.com>
2024-10-22 23:40:01 -0400

    final

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -4,21 +4,97 @@
 //
 //  Created by Jaali Patterson on 10/22/24.
 //
-
 import SwiftUI
-
 struct ContentView: View {
+
+    @State private var parks: [Park] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(parks) { park in
+                        NavigationLink(value: park) {
+                            ParkRow(park: park)
+                        }
+                    }
+                }
+            }
+            .navigationDestination(for: Park.self) { park in // <-- Add a navigationDestination that reacts to any Park type sent from a Navigation Link
+                ParkDetailView(park: park) // <-- Create a ParkDetailView for the destination, passing in the park
+            }.navigationTitle("National Parks") // <-- Add a navigation bar title
         }
-        .padding()
+        .onAppear(perform: {
+            Task {
+                await fetchParks()
+            }
+        })
+    }
+
+    private func fetchParks() async {
+        // URL for the API endpoint
+        // 👋👋👋 Make sure to replace {YOUR_API_KEY} in the URL with your actual NPS API Key
+        let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=EfUlOeLhp1ciiGuR29K9B2bVSHUue5VLP8X16rkH")!
+        do {
+
+            // Perform an asynchronous data request
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // Decode json data into ParksResponse type
+            let parksResponse = try JSONDecoder().decode(ParksResponse.self, from: data)
+
+            // Get the array of parks from the response
+            let parks = parksResponse.data
+
+            // Print the full name of each park in the array
+            for park in parks {
+                print(park.fullName)
+            }
+
+            // Set the parks state property
+            self.parks = parks
+
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
+struct ParkRow: View {
+    let park: Park
 
+    var body: some View {
+        
+        // Park row
+        Rectangle()
+            .aspectRatio(4/3, contentMode: .fit)
+            .overlay {
+                // TODO: Get image url
+                let image = park.images.first
+                let urlString = image?.url
+                let url = urlString.flatMap { string in
+                    URL(string: string)
+                }
+                
+                // TODO: Add AsyncImage
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Color(.systemGray4)
+                }
+            }
+            .overlay(alignment: .bottomLeading) {
+                Text(park.name)
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding()
+            }
+            .cornerRadius(16)
+            .padding(.horizontal)
+    }
+}
 #Preview {
     ContentView()
 }
--- Parks/ParksResponse.swift
+++ Parks/ParksResponse.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+struct ParksResponse: Codable {
+    
+    let data: [Park]
+    
+}
+struct Park: Codable, Identifiable, Hashable, Equatable{
+    let id: String
+    let fullName: String
+    let description: String
+    let latitude: String
+    let longitude: String
+    let images: [ParkImage]
+    let name: String
+    
+    func hash(into hasher: inout Hasher) { // <-- Add required hash function
+        hasher.combine(id)
+    }
+}
+struct ParkImage: Codable, Identifiable, Equatable { // <-- Add Equatable protocol
+    let title: String
+    let caption: String
+    let url: String
+
+    var id: String {
+        url
+    }
+}
+extension Park {
+    static var mocked: Park {
+        // Provide a fallback or dummy park in case of errors
+        let fallbackPark = Park(id: "0", fullName: "Mock Park", description: "Description not available", latitude: "0", longitude: "0", images: [], name: "Mock Park")
+
+        // Attempt to load the mocked park from the JSON file
+        guard let jsonUrl = Bundle.main.url(forResource: "park_mock", withExtension: "json") else {
+            print("Failed to find park_mock.json file.")
+            return fallbackPark
+        }
+
+        do {
+            let data = try Data(contentsOf: jsonUrl)
+            let park = try JSONDecoder().decode(Park.self, from: data)
+            return park
+        } catch {
+            print("Error decoding Park from JSON: \(error.localizedDescription)")
+            return fallbackPark
+        }
+    }
+}
+
+
+
+
