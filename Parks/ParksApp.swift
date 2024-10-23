465d29348897eb9114a2fc4385142e2604dd8310
Jaali <jmasteremerald@gmail.com>
2024-10-22 20:57:08 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Jaali Patterson on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
